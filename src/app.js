efabae5c3d6ddd286f66c8db96e6707545292cf3
Khoa Huynh <khoawin92@gmail.com>
2018-07-16 22:05:30 -0700

    fixed clearDiv function

--- src/app.js
+++ src/app.js
@@ -22,22 +22,20 @@ document.getElementById('nav-roster').addEventListener('click', () => {
 
 document.getElementById('nav-pairs').addEventListener('click', () => superagent.get('https://code-comrade.now.sh/api/v1/roster/pairs')
 .then(data => {
+  clearDiv();
   let str = '<ul>';
   let pairs = data.body.results;
-  console.log(data.body.results);
   for(let student of pairs) {
     str += `<li>${student}</li>`;
   }
   str +='</ul>';
   let div = document.getElementById('pairs');
-  console.log('div', div);
   div.innerHTML=str;
 }));
 
 function clearDiv() {
-  console.log('we here');
-  console.log('doc', document.getElementsByClassName('clearDiv'));
-  
-  document.getElementsByClassName('clearDiv')[0];
-  console.log('84', document.getElementsByClassName('clearDiv')[0]);
+  let obj = document.getElementsByClassName('clearDiv');
+  Object.entries(obj).forEach(div => {
+    div[1].innerHTML = '';
+  });
 }
